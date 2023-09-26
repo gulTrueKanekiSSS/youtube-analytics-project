ced92f0c4acb83959eb1a74286df34b8d917c9b2
Дмитрий Захаров <zds.vip1221@gmail.com>
2023-09-26 13:41:23 +0300

    gg

--- src/channel.py
+++ src/channel.py
@@ -1,15 +1,71 @@
-from src.imp import pjson, youtube
+from typing import Optional, Dict
+
+from src.imp import pjson, youtube, object_to_dict
 
 
 class Channel:
     """Класс для ютуб-канала"""
-
+    __youtube_api = youtube
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+
+    def __get_items(self) -> Optional[Dict]:
+        return self.print_info().get("items")[0]
+
+    def __get_snippet(self) -> Optional[Dict]:
+        return self.__get_items().get("snippet")
+
+    def __get_statistics(self) -> Optional[Dict]:
+        return self.__get_items().get("statistics")
+    @property
+    def get_channel_id(self):
+        return self.__channel_id
+
+    @property
+    def title(self):
+        return self.__get_snippet().get("title")
+
+    @property
+    def get_description_chanel(self):
+        return self.__get_snippet().get("description")
+
+    @property
+    def url(self):
+        return f"https://www.youtube.com/channel/{self.get_channel_id}"
+
+    @property
+    def get_amount_subs(self):
+        return self.__get_statistics().get("subscriberCount")
+
+    @property
+    def video_count(self):
+        return self.__get_statistics().get("videoCount")
+
+    @property
+    def get_amount_views(self):
+        return self.__get_statistics().get("viewCount")
+
+    @classmethod
+    def get_service(cls):
+        return cls.__youtube_api
+
+    def to_json(self, channel_name):
+        obj = Channel(self.get_channel_id)
+        fields = object_to_dict(obj)
+        print(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!{fields}')
+
+        import json
+
+        json_obj = json.dumps(fields, ensure_ascii=False, indent=4, sort_keys=True)
+        with open(f'{channel_name}', 'w', encoding='utf-8') as f:
+            json.dump(json_obj, f)
+
+        return json_obj
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
 
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=self.get_channel_id, part='snippet,statistics').execute()
         pjson(channel)
+        return channel
--- src/imp.py
+++ src/imp.py
@@ -13,3 +13,19 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 def pjson(data: dict) -> None:
     print(json.dumps(data, indent=2, ensure_ascii=False))
+
+def object_to_dict(object):
+    fields = dict()
+    fields.update(object.__class__.__dict__)
+    fields.update(object.__dict__)
+
+    fields = dict(filter(lambda x: not x[0].startswith('_'), fields.items()))
+
+    new_fields = dict()
+    for k, v in fields.items():
+        if hasattr(v, '__dict__'):
+            v = object_to_dict(v)
+
+        new_fields[k] = v
+
+    return new_fields
