f22d522428b3386ef68d11b4cf3e4b3753a8319d
Дмитрий Захаров <zds.vip1221@gmail.com>
2023-09-28 09:14:10 +0300

    gg

--- src/channel.py
+++ src/channel.py
@@ -5,10 +5,13 @@ from src.imp import pjson, youtube, object_to_dict
 
 class Channel:
     """Класс для ютуб-канала"""
+
     __youtube_api = youtube
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
+        self._channel = youtube.channels().list(id=self.get_channel_id, part='snippet,statistics').execute()
 
     def __get_items(self) -> Optional[Dict]:
         return self.print_info().get("items")[0]
@@ -51,21 +54,10 @@ class Channel:
         return cls.__youtube_api
 
     def to_json(self, channel_name):
-        obj = Channel(self.get_channel_id)
-        fields = object_to_dict(obj)
-        print(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!{fields}')
-
-        import json
-
-        json_obj = json.dumps(fields, ensure_ascii=False, indent=4, sort_keys=True)
-        with open(f'{channel_name}', 'w', encoding='utf-8') as f:
-            json.dump(json_obj, f)
-
-        return json_obj
+        with open(channel_name, 'w', encoding='utf-8') as file:
+            object_to_dict(self._channel, file)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-
-        channel = youtube.channels().list(id=self.get_channel_id, part='snippet,statistics').execute()
-        pjson(channel)
-        return channel
+        pjson(self._channel)
+        return self._channel
--- src/imp.py
+++ src/imp.py
@@ -1,5 +1,7 @@
 import os
 import json
+from typing import Dict
+
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
@@ -14,18 +16,8 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 def pjson(data: dict) -> None:
     print(json.dumps(data, indent=2, ensure_ascii=False))
 
-def object_to_dict(object):
-    fields = dict()
-    fields.update(object.__class__.__dict__)
-    fields.update(object.__dict__)
 
-    fields = dict(filter(lambda x: not x[0].startswith('_'), fields.items()))
+def object_to_dict(data: Dict, channel_name):
+    json.dump(data, channel_name, indent=2, ensure_ascii=False)
 
-    new_fields = dict()
-    for k, v in fields.items():
-        if hasattr(v, '__dict__'):
-            v = object_to_dict(v)
 
-        new_fields[k] = v
-
-    return new_fields
